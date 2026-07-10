1fea5731bc63b2e9c491eaa2ff9d0f39f9f45cf1
byu-rin <simon3397@gmail.com>
2026-07-10 16:51:48 +0900

    [new] 값 바뀔 때 효과

--- my-app/src/components/Counter.jsx
+++ my-app/src/components/Counter.jsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("count 바뀜:", count);
+  }, [count]);
+
   return <button onClick={() => setCount((c) => c + 1)}>click {count}</button>;
 };
 export default Counter;
